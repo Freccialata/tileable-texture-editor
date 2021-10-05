f86dbd10aca00ca233514ad1add37428aa9f7a03
Gianluca Rubino <33099442+Freccialata@users.noreply.github.com>
2021-10-05 13:09:33 +0300

    started another tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,20 @@
 /*
-* Tutorial from: https://www.youtube.com/watch?v=HGHbcRscFsg&t=670s
+* tutorial from: https://www.youtube.com/watch?v=028GNYC32Rg
 */
-#include <iostream>
-#include <fstream>
-#include <ctime>
-#include <cstdlib>
 
-#include <sstream>
+#include "Image.h"
 
-using namespace std;
+int main(int argc, char** argv) {
+	Image test("test.jpg");
+	test.write("new.png");
+	Image copy = test;
 
-int main()
-{
-	ifstream image;
-	ofstream newimage;
-
-	image.open("Monument.ppm");
-	newimage.open("newImage.ppm");
-
-	// Copy over header information
-	if ( image.is_open() && newimage.is_open() )
-	{
-		string type = "", width = "", height = "", RGB = "";
-		image >> type;
-		image >> width;
-		image >> height;
-		image >> RGB;
-
-		newimage << type << "\n" << width << " " << height << "\n" << RGB << "\n";
-		// cout << type << " " << width << " " << height << " " << RGB << "\n";
-		
-		string red = "", green = "", blue = "";
-		int r = 0, g = 0, b = 0;
-		while (!image.eof()) {
-			image >> red;
-			image >> green;
-			image >> blue;
-
-			stringstream redstream(red);
-			stringstream greenstream(green);
-			stringstream bluestream(blue);
-
-			redstream >> r;
-			greenstream >> g;
-			bluestream >> b;
-
-			if (b += 50 >= 255)
-			{
-				b = 255;
-			}
-			else
-			{
-				b += 50;
-			}
-
-			newimage << r << " " << g << " " << b << endl;
-		}
-
-		image.close();
-		newimage.close();
+	for (int i = 0; copy.w * copy.channels; i++) {
+		copy.data[i] = 255;
 	}
-	else {
-		return -1;
-	}
+	copy.write("copy.png");
+	Image blank(100, 100, 3);
+	blank.write("blank.jpg");
 
-	// system("pause>0");
 	return 0;
-
-	/*ofstream image;
-	image.open("./src/image.ppm");
-
-	srand(time(0));
-	if (image.is_open())
-	{
-		// Place header info
-		image << "P3\n";
-		image << "250 250\n";
-		image << "255\n";
-
-		for (int y = 0; y < 250; y++)
-		{
-			for (int x = 0; x < 250; x++)
-			{
-				image << rand() % 255 << " " << rand() % 255 << " " << rand() % 255 << endl;
-			}
-		}
-	}
-
-	image.close();*/
 }
