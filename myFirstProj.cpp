5d3df082ae37b2926c71161be301c1f5e07c266e
Gianluca Rubino <33099442+Freccialata@users.noreply.github.com>
2021-10-05 11:21:04 +0300

    init project

---
+++ myFirstProj.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // cout << "Hello World!\n";
+
+    /*float annualSalary;
+    
+    cout << "Please enter your annual salary: ";
+    cin >> annualSalary;
+
+    float monthSalary = annualSalary / 12;
+
+    cout << "Your monthly salary is " << monthSalary;
+    cout << "\nIn 10y you will earn: " << annualSalary * 10;
+
+    char character = 'a';*/
+
+
+
+
+    system("pause>0");
+}
