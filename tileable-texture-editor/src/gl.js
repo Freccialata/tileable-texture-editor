89eba86df6ceefc5c41bcdfcbd7f89927838397d
Gianluca Rubino <33099442+Freccialata@users.noreply.github.com>
2021-11-25 19:53:17 +0200

    second version of the working editor

--- tileable-texture-editor/src/gl.js
+++ tileable-texture-editor/src/gl.js
@@ -17,7 +17,7 @@ const tgl = {'init': function(gl) {
     }
     
     tgl.Texture = function(width, height, channels, isFloat, isLinear, isClamped, texels) {
-        var coordMode = isClamped ? gl.CLAMP_TO_EDGE : gl.REPEAT;
+        const coordMode = isClamped ? gl.CLAMP_TO_EDGE : gl.REPEAT;
         this.type     = isFloat   ? gl.FLOAT         : gl.UNSIGNED_BYTE;
         this.format   = [gl.LUMINANCE, gl.RG, gl.RGB, gl.RGBA][channels - 1];
         
@@ -38,7 +38,7 @@ const tgl = {'init': function(gl) {
     }
     
     tgl.Texture.prototype.setSmooth = function(smooth) {
-        var interpMode = smooth ? gl.LINEAR : gl.NEAREST;
+        const interpMode = smooth ? gl.LINEAR : gl.NEAREST;
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, interpMode);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, interpMode);
     }
@@ -79,15 +79,15 @@ const tgl = {'init': function(gl) {
     }
     
     tgl.Shader.prototype.createShaderObject = function(name, isFragment) {
-        var shaderSource = this.resolveShaderSource(name);
-        var shaderObject = gl.createShader(isFragment ? gl.FRAGMENT_SHADER : gl.VERTEX_SHADER);
+        const shaderSource = this.resolveShaderSource(name);
+        const shaderObject = gl.createShader(isFragment ? gl.FRAGMENT_SHADER : gl.VERTEX_SHADER);
         gl.shaderSource(shaderObject, shaderSource);
         gl.compileShader(shaderObject);
 
         if (!gl.getShaderParameter(shaderObject, gl.COMPILE_STATUS)) {
             /* Add some line numbers for convenience */
-            var lines = shaderSource.split("\n");
-            for (var i = 0; i < lines.length; ++i)
+            const lines = shaderSource.split("\n");
+            for (let i = 0; i < lines.length; ++i)
                 lines[i] = ("   " + (i + 1)).slice(-4) + " | " + lines[i];
             shaderSource = lines.join("\n");
         
@@ -102,15 +102,15 @@ const tgl = {'init': function(gl) {
     }
     
     tgl.Shader.prototype.resolveShaderSource = function(name) {
-        var element = document.getElementById(name);
+        const element = document.getElementById(name);
         if (!element)
             throw new Error("Unable to find shader source for '" + name + "'");
-        var shaderSource = element.textContent;
+        const shaderSource = element.textContent;
         
         /* Rudimentary include handling for convenience.
            Not the most robust, but it will do for our purposes */
-        var pattern = new RegExp('#include "(.+)"');
-        var match;
+        const pattern = new RegExp('#include "(.+)"');
+        let match;
         while (match = pattern.exec(shaderSource)) {
             shaderSource = shaderSource.slice(0, match.index) +
                            this.resolveShaderSource(shaderDict, match[1]) +
@@ -127,31 +127,31 @@ const tgl = {'init': function(gl) {
     }
     
     tgl.Shader.prototype.uniformTexture = function(name, texture) {
-        var id = this.uniformIndex(name);
+        let id = this.uniformIndex(name);
         if (id != -1)
             gl.uniform1i(id, texture.boundUnit);
     }
     
     tgl.Shader.prototype.uniformI = function(name, f) {
-        var id = this.uniformIndex(name);
+        let id = this.uniformIndex(name);
         if (id != -1)
             gl.uniform1i(id, f);
     }
     
     tgl.Shader.prototype.uniformF = function(name, f) {
-        var id = this.uniformIndex(name);
+        let id = this.uniformIndex(name);
         if (id != -1)
             gl.uniform1f(id, f);
     }
     
     tgl.Shader.prototype.uniform2F = function(name, f1, f2) {
-        var id = this.uniformIndex(name);
+        let id = this.uniformIndex(name);
         if (id != -1)
             gl.uniform2f(id, f1, f2);
     }
     
     tgl.Shader.prototype.uniformMatrix2F = function(name, f1, f2, f3, f4) {
-        var id = this.uniformIndex(name);
+        let id = this.uniformIndex(name);
         if (id != -1)
             gl.uniformMatrix2fv(id, false, [f1, f3, f2, f4]);
     }
@@ -191,10 +191,10 @@ const tgl = {'init': function(gl) {
     }
     
     tgl.VertexBuffer.prototype.draw = function(shader, mode, length) {
-        for (var i = 0; i < this.attributes.length; ++i) {
+        for (let i = 0; i < this.attributes.length; ++i) {
             this.attributes[i].index = gl.getAttribLocation(shader.program, this.attributes[i].name);
             if (this.attributes[i].index >= 0) {
-                var attr = this.attributes[i];
+                let attr = this.attributes[i];
                 gl.enableVertexAttribArray(attr.index);
                 gl.vertexAttribPointer(attr.index, attr.size, attr.type, attr.norm, this.elementSize, attr.offset);
             }
@@ -202,7 +202,7 @@ const tgl = {'init': function(gl) {
         
         gl.drawArrays(mode, 0, length ? length : this.length);
         
-        for (var i = 0; i < this.attributes.length; ++i) {
+        for (let i = 0; i < this.attributes.length; ++i) {
             if (this.attributes[i].index >= 0) {
                 gl.disableVertexAttribArray(this.attributes[i].index);
                 this.attributes[i].index = -1;
@@ -213,18 +213,18 @@ const tgl = {'init': function(gl) {
 
 const MathFunc = {
     erf(x) {
-        var a1 =  0.254829592;
-        var a2 = -0.284496736;
-        var a3 =  1.421413741;
-        var a4 = -1.453152027;
-        var a5 =  1.061405429;
-        var p  =  0.3275911;
+        const a1 =  0.254829592;
+        const a2 = -0.284496736;
+        const a3 =  1.421413741;
+        const a4 = -1.453152027;
+        const a5 =  1.061405429;
+        const p  =  0.3275911;
         
-        var sign = x < 0 ? -1 : 1;
+        const sign = x < 0 ? -1 : 1;
         x = Math.abs(x);
         
-        var t = 1.0/(1.0 + p*x);
-        var y = 1.0 - ((((a5*t + a4)*t + a3)*t + a2)*t + a1)*t*Math.exp(-x*x);
+        let t = 1.0/(1.0 + p*x);
+        let y = 1.0 - ((((a5*t + a4)*t + a3)*t + a2)*t + a1)*t*Math.exp(-x*x);
         
         return sign*y;
     },
@@ -234,7 +234,7 @@ const MathFunc = {
     },
     
     erfInv(x) {
-        var y = 0.0, err;
+        let y = 0.0, err;
         do {
             err = this.erf(y) - x;
             y -= err/this.derf(y);
@@ -256,22 +256,5 @@ const MathFunc = {
 
     truncCdfInv(x, sigma) {
         return 0.5 + Math.sqrt(2)*sigma*this.erfInv((2.0*x - 1.0)/this.C(sigma));
-    }
-}
-
-const HelperFunc = {
-    showImageWithCanvas(imageData, w, h, parentElement, canvasId = 'my-canvas') {
-        // TEMP Show the state of the image before passing it to tgl.texture
-        let myCanvas = document.getElementById(canvasId);
-        if (!myCanvas) {
-            myCanvas = document.createElement('canvas');
-            myCanvas.id = canvasId;
-        }
-        myCanvas.width = w;
-        myCanvas.height = h;
-        const myCtx = myCanvas.getContext('2d');
-        myCtx.putImageData(imageData, 0, 0);
-        parentElement.appendChild(myCanvas);
-        parentElement.replaceChild(myCanvas, myCanvas);
     }
 }
--- tileable-texture-editor/src/renderer.js
+++ tileable-texture-editor/src/renderer.js
@@ -1,32 +1,27 @@
-// TODO check exactly how the image should be uploaded
-
 class SeamlessTiling {
     constructor() {
         this.container_canvas = document.getElementById('canvas-container');
         this.canvas = document.createElement('canvas');
         this.canvas.id = "render-result";
-        this.original_pixelData = null;
         this.img_name = "pattern-rock.png"; // Starting default name
+        
         this.applyOnClickFunctions();
-
         try {
             this.setupGL();
         } catch (e) {
-            console.error(e.message + ". This app won't run properly.");
+            console.error(e + ". This app won't run properly. Error in setting up GL.");
         }
-
         try {
-            this.startRenderPipeline();
+            this.container_canvas.appendChild(this.canvas);
+            this.loadImage(this.img_name);
         } catch (e) {
-            console.error(e);
+            console.error("Error in the image processing:\n", e);
         }
     }
 
     applyOnClickFunctions() {
         document.getElementById("select-img-btn").addEventListener("click", () =>{this.selectimage()});
         document.getElementById("dwl-img-btn").addEventListener("click", ()=>{this.downloadImg()});
-        document.getElementById("clear-canvas-btn").addEventListener("click", ()=>{this.clearcanvas()});
-        // document.getElementById("revert-btn").addEventListener("click", ()=>{this.revertToOriginalImage()});
     }
 
     setupGL() {
@@ -52,11 +47,8 @@ class SeamlessTiling {
         
         this.randRes = 512;
         
-        this.time = 0;
-        this.speed = 0; // MODIF Default=1
-        
-        var texels = new Uint8Array(this.randRes*this.randRes*4);
-        for (var i = 0; i < this.randRes*this.randRes*4; ++i)
+        let texels = new Uint8Array(this.randRes*this.randRes*4);
+        for (let i = 0; i < this.randRes*this.randRes*4; ++i)
             texels[i] = Math.random()*256.0;
         
         this.randTex = new tgl.Texture(this.randRes, this.randRes, 4, false, false, false, texels);
@@ -67,69 +59,6 @@ class SeamlessTiling {
             [- 37.8/255.0, - 74.2/255.0,  112.0/255.0],
             [ 112.0/255.0, - 93.8/255.0, - 18.2/255.0]
         ];
-    }
-
-    startRenderPipeline() {
-        this.loadImage(this.img_name);
-        this.container_canvas.appendChild(this.canvas)
-        /*let renderer = this;
-        
-        let patterns = document.getElementById("patterns").childNodes;
-        let activePattern = null;
-        for (let i = 0; i < patterns.length; ++i) {
-            if (patterns[i] instanceof HTMLImageElement) {
-                if (!activePattern)
-                    activePattern = patterns[i];
-                
-                patterns[i].addEventListener("click", (function(upload) {
-                    if (upload)
-                        document.getElementById('file-input').click();
-                    else
-                        renderer.preparePattern(this);
-                    addClass(this, "active");
-                    if (activePattern)
-                        stripClass(activePattern, "active");
-                    activePattern = this;
-                }).bind(patterns[i], i == patterns.length - 2));
-            }
-        }
-        this.preparePattern(activePattern);
-        
-        document.getElementById('file-input').addEventListener('change', function() {
-            if (!this.files)
-                return;
-            let img = document.getElementById('img-upload');
-            img.src = URL.createObjectURL(this.files[0]);
-            img.onload = function() {
-                renderer.preparePattern(this);
-            };
-        });
-        
-        document.body.addEventListener('dragover', function(ev) {
-            ev.preventDefault();
-        });
-        document.body.addEventListener('drop', function(ev) {
-            ev.preventDefault();
-            
-            let file;
-            
-            if (ev.dataTransfer.items) {
-                if (ev.dataTransfer.items[0].kind == 'file')
-                    file = ev.dataTransfer.items[0].getAsFile();
-            } else if (ev.dataTransfer.files) {
-                file = ev.dataTransfer.files[0];
-            }
-            
-            let reader = new FileReader();
-            let img = document.getElementById('img-upload');
-            reader.readAsDataURL(file);
-            reader.onload = function(f) {
-                img.src = f.target.result;
-                img.onload = function() {
-                    renderer.preparePattern(this);
-                };
-            };
-        });*/
     }
 
     createQuadVbo() {
@@ -147,6 +76,35 @@ class SeamlessTiling {
         return vbo;
     }
 
+    selectimage() {
+        const reader = new FileReader()
+        const file_upload_elem = document.getElementById("foto-file");
+        
+        file_upload_elem.click();
+    
+        file_upload_elem.onchange = () => {
+            this.img_name = file_upload_elem.files.item(0).name;
+            console.log(this.img_name);
+            reader.readAsDataURL(file_upload_elem.files[0])
+            reader.onload = () => {
+                this.loadImage(reader.result);
+            }
+        }
+    }
+
+    loadImage(img_path) {
+        if (img_path.length > 1) {
+            const image = new Image();
+            image.src = img_path;
+            image.onload = () => {
+                this.preparePattern(image);
+            }
+        }
+        else {
+            throw new Error("Image path not set or not valid: " + img_path);
+        }
+    }
+
     preparePattern(pattern) {
         // NOTE the image is cropped to be fitted into a (Power of two?) specific resolution
         let minSize = Math.min(pattern.naturalWidth, pattern.naturalHeight);
@@ -159,9 +117,6 @@ class SeamlessTiling {
         this.imageCanvas.height = h;
         this.imageContext.drawImage(pattern, 0, 0);
         let data = this.imageContext.getImageData(0, 0, w, h);
-
-        // Show the uploaded image after cropping and before the histogram algorithm
-        HelperFunc.showImageWithCanvas(data, w, h, this.container_canvas);
         
         let histo = new Uint32Array(256);
         for (let i = 0; i < 256; ++i)
@@ -216,46 +171,11 @@ class SeamlessTiling {
         this.renderOnScreen();
     }
 
-    loadImage(img_path) {
-        if (img_path.length > 1) {
-            const image = new Image();
-            image.src = img_path;
-            image.onload = () => {
-                this.preparePattern(image);
-                /*this.canvas.width = image.naturalWidth;
-                this.canvas.height = image.naturalHeight;
-                this.ctx.drawImage(image, 0, 0);
-                this.container_canvas.innerHTML = "";
-                this.container_canvas.appendChild(this.canvas);
-                this.original_pixelData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);*/
-            }
-        }
-        else {
-            throw new Error("Image path not set or not valid: " + img_path);
-        }
-    }
-
-    selectimage() {
-        const reader = new FileReader()
-        const file_upload_elem = document.getElementById("foto-file");
-        
-        file_upload_elem.click();
-    
-        file_upload_elem.onchange = () => {
-            this.img_name = file_upload_elem.files.item(0).name;
-            console.log(this.img_name);
-            reader.readAsDataURL(file_upload_elem.files[0])
-            reader.onload = () => {
-                this.loadImage(reader.result);
-            }
-        }
-    }
-
     renderOnScreen() {
         const gl = this.gl;
         
-        let windowW =  (window.innerWidth > 0) ? window.innerWidth : screen.width;
-        this.canvas.width = Math.min(windowW, 512); // Default: 820
+        // let windowW =  (window.innerWidth > 0) ? window.innerWidth : screen.width;
+        // this.canvas.width = Math.min(windowW, 820); // Default: 820
         
         gl.clear(gl.COLOR_BUFFER_BIT);
         
@@ -285,40 +205,24 @@ class SeamlessTiling {
         this.compositeProgram.uniformMatrix2F("LatticeToWorld", a, b, c, d);
         this.compositeProgram.uniformMatrix2F("WorldToLattice", d/det, -b/det, -c/det, a/det);
         this.quadVbo.draw(this.compositeProgram, this.gl.TRIANGLE_FAN);
-    }
 
-    clearcanvas() {
-        this.container_canvas.innerHTML = "";
-        // this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.original_pixelData = null;
-        this.img_name = "";
-    }
-
-    revertToOriginalImage() {
-        this.ctx.putImageData(this.original_pixelData, 0, 0);
+        // Store the drawn result in a context 2d canvas in order to save it
+        this.outputcanvas = document.createElement("canvas");
+        this.outputcanvas.getContext('2d').drawImage(this.canvas, 0, 0);
     }
 
     downloadImg() {
-        // Download the image only if the canvas is not empty
-        /*if (this.gl.getImageData(0, 0, this.canvas.width, this.canvas.height).data
+        // Download the image only if the 2d canvas exists or it is not empty
+        if (this.outputcanvas || this.outputcanvas.getContext('2d').getImageData(0, 0, this.canvas.width, this.canvas.height).data
             .some(channel => channel !== 0)) {
             let link = document.createElement('a');
             link.download = this.img_name + '-edited.png';
-            link.href = this.canvas.toDataURL();
+            link.href = this.outputcanvas.toDataURL();
             link.click();
         }
         else {
             throw new Error("No image to download");
-        }*/
-        const tempcanvas = document.createElement("canvas")
-        const tctx = tempcanvas.getContext('2d');
-        tctx.drawImage(this.canvas, 0, 0);
-        const capturedImage = tempcanvas.toDataURL();
-
-        let link = document.createElement('a');
-        link.download = this.img_name + '-edited.png';
-        link.href = capturedImage;
-        link.click();
+        }
     }
 }
 
@@ -326,5 +230,4 @@ class SeamlessTiling {
 window.onload = () => {
     // MAIN
     const Evironment = new SeamlessTiling()
-    // Evironment.loadImage("./pattern-rock.png")
 }
